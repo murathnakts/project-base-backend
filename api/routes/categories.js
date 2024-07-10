6e2fdfc91d058135c07af6c9d0a8b6384865268f
murathnakts <murathanaktas111002@gmail.com>
2024-07-10 17:47:05 +0300

    endpoints

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const Categories = require("../db/models/Categories");
+const Response = require("../lib/Response");
+const CustomError = require("../lib/Error");
+const Enum = require("../config/Enum");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET categories listing. */
+router.get('/', async (req, res) => {
+
+  try {
+    let categories = await Categories.find({});
+    res.json(Response.successResponse(categories));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error)
+    res.status(errorResponse.code).json(errorResponse);
+  }
 });
+
+router.post("/add", async (req, res) => {
+  let body = req.body
+  try {
+
+    if (!body.name) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error!", "name field must be filled");
+
+    let category = new Categories({
+      name: body.name,
+      is_active: true,
+      created_by: req.user?.id
+    });
+
+    await category.save();
+
+    res.json(Response.successResponse({ success: true }));
+
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error)
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/update", async (req, res) => {
+  try {
+    
+    //28.00
+
+  } catch (error) {
+    
+  }
+})
 
 module.exports = router;
